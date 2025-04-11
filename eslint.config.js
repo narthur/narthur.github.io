41b50686c62d0c5db94dbed88aa64efa253b2cc1
Nathan Arthur <nathan@nathanarthur.com>
2025-04-11 13:01:51 -0400

    eslint config update

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
@@ -22,7 +23,7 @@ export default ts.config(
 			}
 		},
 		rules: {
-			'@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }]
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
 		}
 	},
 	{
